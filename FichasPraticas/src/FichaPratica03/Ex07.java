5fee01b7211afe84e7f13b34bee41ceee938f7b7
Vitor Santos <vmvs007@gmail.com>
2026-04-09 09:14:16 +0100

    Create Ex07.java

---
+++ FichasPraticas/src/FichaPratica03/Ex07.java
@@ -0,0 +1,20 @@
+package FichaPratica03;
+
+public class Ex07 {
+    public static void main(String[] args) {
+
+        // Declarar variáveis
+        int num = 1, somatorio = 0;
+
+        while (num <= 100) {
+            System.out.println(num);
+
+            somatorio = somatorio + num;
+
+            num++; // num = num + 1;
+        }
+
+        System.out.println("Somatório: "+somatorio);
+
+    }
+}
