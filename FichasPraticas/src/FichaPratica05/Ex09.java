6258ad36d9cb54d5e4333be735d611d2d67c332e
Vitor Santos <vmvs007@gmail.com>
2026-04-16 16:55:54 +0100

    FP05

--- FichasPraticas/src/FichaPratica05/Ex09.java
+++ FichasPraticas/src/FichaPratica05/Ex09.java
@@ -2,7 +2,7 @@ package FichaPratica05;
 
 import java.util.Scanner;
 
-public class Ex08 {
+public class Ex09 {
     public static void main(String[] args) {
 
         // Import do Scanner
@@ -19,13 +19,15 @@ public class Ex08 {
             }
         }
 
-        // Impressão da matriz
+        int soma = 0;
+
         for (int linha = 0; linha < matriz.length; linha++) {
             for (int coluna = 0; coluna < matriz[0].length; coluna++) {
-                System.out.print(matriz[linha][coluna] + "  ");
+                soma = soma + matriz[linha][coluna];
             }
-            System.out.println();
         }
+
+        System.out.println("Soma dos Elementos: " + soma);
 
 
     }
--- FichasPraticas/src/FichaPratica05/Ex10.java
+++ FichasPraticas/src/FichaPratica05/Ex10.java
@@ -2,14 +2,14 @@ package FichaPratica05;
 
 import java.util.Scanner;
 
-public class Ex08 {
+public class Ex10 {
     public static void main(String[] args) {
 
         // Import do Scanner
         Scanner input = new Scanner(System.in);
 
         // Criar variáveis
-        int[][] matriz = new int[3][3];
+        int[][] matriz = new int[3][5];
 
         // Leitura da matriz
         for (int linha = 0; linha < matriz.length; linha++) {
@@ -17,16 +17,26 @@ public class Ex08 {
                 System.out.print("Insira na matriz[" + linha + "][" + coluna + "]: ");
                 matriz[linha][coluna] = input.nextInt();
             }
-        }
-
-        // Impressão da matriz
-        for (int linha = 0; linha < matriz.length; linha++) {
-            for (int coluna = 0; coluna < matriz[0].length; coluna++) {
-                System.out.print(matriz[linha][coluna] + "  ");
-            }
             System.out.println();
         }
 
+        // Perguntar o número a pesquisar
+        int numPesquisa, contadorOcorrencias = 0;
+
+        System.out.print("Número que quer pesquisar: ");
+        numPesquisa = input.nextInt();
+
+        // Contar quantas vezes é que o numPesquisa aparece na matriz
+        for (int linha = 0; linha < matriz.length; linha++) {
+            for (int coluna = 0; coluna < matriz[0].length; coluna++) {
+
+                if (matriz[linha][coluna] == numPesquisa) {
+                    contadorOcorrencias++;
+                }
+            }
+        }
+
+        System.out.println("O número " + numPesquisa + " aparece " + contadorOcorrencias + " vezes na matriz");
 
     }
 }
