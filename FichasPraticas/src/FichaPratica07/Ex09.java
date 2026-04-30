65b0ab2550661d1f2cfdde1b781e1a5640ff342a
Vitor Santos <vmvs007@gmail.com>
2026-04-30 17:09:41 +0100

    Create Ex09.java

---
+++ FichasPraticas/src/FichaPratica07/Ex09.java
@@ -0,0 +1,182 @@
+package FichaPratica07;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Ex09 {
+
+    /**
+     * Função que conta o número de linhas de um ficheiro
+     *
+     * @param caminhoFicheiro Caminho para o ficheiro
+     * @return Número de linhas totais
+     * @throws FileNotFoundException Caso o ficheiro não exista
+     */
+    public static int contarLinhasFicheiro(String caminhoFicheiro) throws FileNotFoundException {
+
+        File ficheiro = new File(caminhoFicheiro);
+        Scanner sc = new Scanner(ficheiro);
+
+        int contagemLinhas = 0;
+
+        // Enquanto o Scanner tiver uma linha para a frente
+        while (sc.hasNextLine()) {
+            // Avançamos a linha atual
+            sc.nextLine();
+
+            // Contamos +1
+            contagemLinhas++;
+        }
+
+        return contagemLinhas;
+
+    }
+
+    public static int contarColunasFicheiro(String caminhoFicheiro) throws FileNotFoundException {
+
+        File ficheiro = new File(caminhoFicheiro);
+        Scanner sc = new Scanner(ficheiro);
+
+        String linha = sc.nextLine();
+        String[] linhaSeparada = linha.split(",");
+
+        int contagemColunas = linhaSeparada.length;
+
+        return contagemColunas;
+    }
+
+    public static String[][] lerFicheiroParaMatriz(String caminhoFicheiro) throws FileNotFoundException {
+
+        int numeroLinhas = contarLinhasFicheiro(caminhoFicheiro) - 1;
+        int numeroColunas = contarColunasFicheiro(caminhoFicheiro);
+
+        int linhaAtualMatriz = 0;
+
+        String[][] matrizCompleta = new String[numeroLinhas][numeroColunas];
+
+        File ficheiro = new File(caminhoFicheiro);
+        Scanner sc = new Scanner(ficheiro);
+
+        // Avançar o cabeçalho
+        sc.nextLine();
+
+        while (sc.hasNextLine()) {
+            String linha = sc.nextLine();
+            String[] linhaSeparada = linha.split(",");
+
+//            matrizCompleta[linhaAtualMatriz][0] = linhaSeparada[0];
+//            matrizCompleta[linhaAtualMatriz][1] = linhaSeparada[1];
+//            matrizCompleta[linhaAtualMatriz][2] = linhaSeparada[2];
+//            matrizCompleta[linhaAtualMatriz][3] = linhaSeparada[3];
+
+            for (int i = 0; i < numeroColunas; i++) {
+                matrizCompleta[linhaAtualMatriz][i] = linhaSeparada[i];
+            }
+
+            linhaAtualMatriz++;
+        }
+
+        return matrizCompleta;
+
+    }
+
+    public static void pesquisarMusicasPorGenero(String[][] matrizMusicas, String generoPesquisar) {
+
+        for (int linha = 0; linha < matrizMusicas.length; linha++) {
+            if (matrizMusicas[linha][2].equalsIgnoreCase(generoPesquisar)) {
+                System.out.println(matrizMusicas[linha][0] + "||" + matrizMusicas[linha][1]);
+            }
+        }
+    }
+
+    public static void pesquisarMusicasPorArtista(String[][] matrizMusicas, String artistaPesquisar) {
+
+        for (int linha = 0; linha < matrizMusicas.length; linha++) {
+            if (matrizMusicas[linha][1].equalsIgnoreCase(artistaPesquisar)) {
+                System.out.println(matrizMusicas[linha][0] + "||" + matrizMusicas[linha][1]);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String[][] matrizMusicas = lerFicheiroParaMatriz("Ficheiros/exercicio_09.csv");
+
+        // Impressão da matriz
+        for (int linha = 0; linha < matrizMusicas.length; linha++) {
+            for (int coluna = 0; coluna < matrizMusicas[0].length; coluna++) {
+                System.out.print(matrizMusicas[linha][coluna] + " || ");
+            }
+            System.out.println();
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("\n\n\n------------------------------------------------");
+            System.out.println("----- Menu de Análise de Playlists -----");
+            System.out.println("1. Pesquisar por músicas de um determinado género");
+            System.out.println("2. Pesquisar músicas de um determinado artista");
+            System.out.println("3. Pesquisar música com maior duração");
+            System.out.println("4. Pesquisar músicas com duração acima de um valor especificado");
+            System.out.println("5. Número de músicas da playlist");
+            System.out.println("0. Sair");
+
+            System.out.print("Opção: ");
+            opcao = input.nextInt();
+
+            System.out.println("\n\n\n------------------------------------------------");
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("1. Pesquisar por músicas de um determinado género");
+
+                    System.out.print("Género a pesquisar: ");
+                    input.nextLine(); // Limpar buffer
+                    String generoPesquisar = input.nextLine();
+
+                    pesquisarMusicasPorGenero(matrizMusicas,generoPesquisar);
+
+
+                    break;
+
+                case 2:
+                    System.out.println("2. Pesquisar músicas de um determinado artista");
+
+
+                    System.out.print("Artista a pesquisar: ");
+                    input.nextLine(); // Limpar buffer
+                    String artistaPesquisar = input.nextLine();
+
+                    pesquisarMusicasPorArtista(matrizMusicas,artistaPesquisar);
+
+                    break;
+
+                case 3:
+                    System.out.println("3. Pesquisar música com maior duração");
+                    break;
+
+                case 4:
+                    System.out.println("4. Pesquisar músicas com duração acima de um valor especificado");
+                    break;
+
+                case 5:
+                    System.out.println("5. Número de músicas da playlist");
+                    break;
+
+                case 0:
+                    System.out.println("0. Sair");
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida: " + opcao);
+                    break;
+            }
+
+        } while (opcao != 0);
+
+    }
+}
