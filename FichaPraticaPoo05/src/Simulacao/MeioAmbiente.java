e1a132c0835ef8d7e8e2f2e29408271818c2bc13
Vitor Santos <vmvs007@gmail.com>
2026-06-09 15:19:36 +0100

    FP05

--- FichaPraticaPoo05/src/Simulacao/MeioAmbiente.java
+++ FichaPraticaPoo05/src/Simulacao/MeioAmbiente.java
@@ -31,7 +31,7 @@ public class MeioAmbiente {
         this.seresVivos.add(serVivoNovo);
     }
 
-    public void removerSerVivo(SerVivo serVivoRemover){
+    public void removerSerVivo(SerVivo serVivoRemover) {
         this.seresVivos.remove(serVivoRemover);
     }
 
@@ -76,9 +76,9 @@ public class MeioAmbiente {
             }
         }
 
-        if(listaIndexesInsetos.isEmpty()){
+        if (listaIndexesInsetos.isEmpty()) {
             return -1;
-        }else{
+        } else {
             Random rd = new Random();
             int insetoSorteado = rd.nextInt(listaIndexesInsetos.size());
 
@@ -103,70 +103,80 @@ public class MeioAmbiente {
         }
     }
 
+    public void exibirSeresVivos() {
+        System.out.println("-+-+-+-+-+-+-+-+-+-+-+- Seres Vivos que ainda estão no/a " + this.nome + " -+-+-+-+-+-+-+-+-+-+-+-");
+        for (SerVivo serVivoAtual : this.seresVivos) {
+            serVivoAtual.exibirDetalhes();
+        }
+    }
+
     /**
      * Comportamento quando uma planta abana com o vento
+     *
      * @param indexPlanta Index da planta que vai abanar
      */
-    public void plantaAbanaComVento(int indexPlanta){
+    public void plantaAbanaComVento(int indexPlanta) {
         // TODO: Método não desenvolvido
     }
 
     /**
      * Comportamento quando um animal faz o seu barulho característico
+     *
      * @param indexAnimal Index do animal que vai fazer o barulho
      */
-    public void animalFazBarulho(int indexAnimal){
+    public void animalFazBarulho(int indexAnimal) {
         // TODO: Método não desenvolvido
     }
 
     /**
      * Comportamento para um animal beber água do meio ambiente
+     *
      * @param indexAnimal Index do animal que vai beber
      * @return true - se o animal bebeu (e continua vivo) | false - se não bebeu e aí morre de sede (é removido do array de Seres Vivos)
      */
-    public boolean animalBebe(int indexAnimal){
+    public boolean animalBebe(int indexAnimal) {
         // TODO: Método não desenvolvido
         return true;
     }
 
-    public boolean animalCome(int indexAnimal){
+    public boolean animalCome(int indexAnimal) {
         // TODO: Método não desenvolvido
         return true;
     }
 
-    public void insetoChateia(int indexInseto){
+    public void insetoChateia(int indexInseto) {
         // TODO: Método não desenvolvido
     }
 
-    public void chuva(){
+    public void chuva() {
         // TODO: Método não desenvolvido
     }
 
-    public void chuvasTorrenciais(){
+    public void chuvasTorrenciais() {
         // TODO: Método não desenvolvido
     }
 
-    public void trovoada(){
+    public void trovoada() {
         // TODO: Método não desenvolvido
     }
 
-    public void seca(){
+    public void seca() {
         // TODO: Método não desenvolvido
     }
 
-    public void erupcaoVulcanica(){
+    public void erupcaoVulcanica() {
         // TODO: Método não desenvolvido
     }
 
-    public void tremorTerra(){
+    public void tremorTerra() {
         // TODO: Método não desenvolvido
     }
 
-    public void incendio(){
+    public void incendio() {
         // TODO: Método não desenvolvido
     }
 
-    public void simulador(){
+    public void simulador() {
         // TODO: Método não desenvolvido
     }
 }
